119204f5048b7518fe2bc230fa009ee03463d215
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-11-19 01:43:37 +0530

    code number 643 updatet

--- src/Code_75/MaxAverageSubarrayCalculator.java
+++ src/Code_75/MaxAverageSubarrayCalculator.java
@@ -7,5 +7,33 @@ public class MaxAverageSubarrayCalculator {
        for (int x : arr){
            System.out.println(x+" ");
        }
+
+
+        System.out.println(maxAvg(arr,k));
+    }
+    public static double maxAvg(int nums[],int k){
+
+        double avg =0.0;
+        double ans =0.0;
+
+        int sum =0;
+
+        for(int i=0;i<k;i++){
+            sum = sum+nums[i];
+        }
+        avg = (sum*1.0)/k;
+        ans =avg;
+
+        for(int i=k;i<nums.length;i++){
+
+            sum = sum+nums[i]-nums[i-k];
+            avg = (sum*1.0)/k;
+
+            ans = Math.max(avg,ans);
+
+        }
+
+        return ans;
+
     }
 }
