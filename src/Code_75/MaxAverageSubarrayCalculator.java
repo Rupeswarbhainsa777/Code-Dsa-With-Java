6b0ab5e7df05a165a81d659fc7d49355a5f4d382
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-11-18 22:53:26 +0530

    code number 643   add

---
+++ src/Code_75/MaxAverageSubarrayCalculator.java
@@ -0,0 +1,11 @@
+package Code_75;
+
+public class MaxAverageSubarrayCalculator {
+    public static void main(String[] args) {
+       int arr[] = {1,12,-5,-6,50,3};
+       int k =4;
+       for (int x : arr){
+           System.out.println(x+" ");
+       }
+    }
+}
