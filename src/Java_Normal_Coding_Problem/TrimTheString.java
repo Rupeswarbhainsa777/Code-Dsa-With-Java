331b4b4e6c99210c0d0209c1007c1c808592867e
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-11-20 20:31:49 +0530

    testing some code

--- src/Java_Normal_Coding_Problem/TrimTheString.java
+++ src/Java_Normal_Coding_Problem/TrimTheString.java
@@ -1,8 +1,16 @@
 package Java_Normal_Coding_Problem;
 
+import java.util.Arrays;
+
 public class TrimTheString {
     public static void main(String[] args) {
+               int arr[] = {1,0,1,0,1,0,1,0};
 
+        Arrays.sort(arr);
+        for(int ar : arr) {
+
+            System.out.println(ar);
+        }
     }
 
 }
