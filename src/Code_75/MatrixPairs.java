c5943ddb91dc3975623a6ab9edd6f446ed122996
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-11-23 21:47:45 +0530

    code no   add

---
+++ src/Code_75/MatrixPairs.java
@@ -0,0 +1,11 @@
+package Code_75;
+
+import java.util.Arrays;
+
+public class MatrixPairs {
+    public static void main(String[] args) {
+                   int arr[] ={1,2,3,4};
+                   String st = Arrays.toString(arr);
+        System.out.println(st);
+    }
+}
