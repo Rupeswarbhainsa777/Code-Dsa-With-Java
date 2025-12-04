1275608a69b1b3006fefc00c674cd9f42a29a1d3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-12-04 23:08:01 +0530

    added

---
+++ src/Java_Normal_Coding_Problem/AddTwoNum.java
@@ -0,0 +1,9 @@
+package Java_Normal_Coding_Problem;
+
+public class AddTwoNum {
+    public static void main(String[] args) {
+        System.out.println(10%2);
+        int dv=10/2;
+        System.out.println(dv);
+    }
+}
