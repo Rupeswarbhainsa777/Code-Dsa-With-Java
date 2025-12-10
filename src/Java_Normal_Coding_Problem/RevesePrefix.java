e87775b663d26a38aef627ae73a4338209a639df
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-12-11 02:52:53 +0530

    leetcode number 2000 updated

--- src/Java_Normal_Coding_Problem/RevesePrefix.java
+++ src/Java_Normal_Coding_Problem/RevesePrefix.java
@@ -4,23 +4,38 @@
 //    public static void main(String[] args) {
 //
 //    }
-//    public String reversePrefix(String word, char ch) {
 //
-//
-//
-//        StringBuilder  res=new StringBuilder();
-//
-//        for(int i=0;i<word.length();i++){
-//
-//            char c=word.charAt(i);
-//            res.insert(0,c);
-//            if(c==ch){
-//                res.append(word.substring(i+1,word.length()));
-//                return res.toString();
-//
+//    char arr[] = word.toCharArray();
+//    int left=0;
+//       for(int right=0;right<word.length();right++){
+//        char c=arr[right];
+//        if(c==ch){
+//            while(left<right){
+//                char temp= arr[right];
+//                arr[right]=   arr[left];
+//                arr[left]=temp;
+//                left++;
+//                right--;
 //            }
+//            return new String(arr);
 //        }
-//        return word;
 //
 //    }
+//
+//return word;
+//     StringBuilder  res=new StringBuilder();
+//
+//     for(int i=0;i<word.length();i++){
+//
+//         char c=word.charAt(i);
+//         res.insert(0,c);
+//         if(c==ch){
+//             res.append(word.substring(i+1,word.length()));
+//             return res.toString();
+//
+//         }
+//     }
+//     return word;
+//
+//}
 //}
