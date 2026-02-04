c3879f1a2ee88ca53015be7006feeda603d3de9c
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-04 23:13:20 +0530

    upated

---
+++ src/JavaDaily/Feb_2.java
@@ -0,0 +1,12 @@
+package JavaDaily;
+
+public class Feb_2 {
+    public static void main(String[] args) {
+        System.out.println();
+        int n =9;
+        do {
+            System.out.println(n);
+        }
+        while (n<0);
+    }
+}
