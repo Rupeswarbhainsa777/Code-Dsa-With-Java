8fbebfe4809745fac7e8dd33ef83e2657e442afe
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-14 12:52:09 +0530

    second pattern problems solve

--- src/Pattern/F14.java
+++ src/Pattern/F14.java
@@ -3,11 +3,23 @@ package Pattern;
 public class F14 {
     public static void main(String[] args) {
 
-
+//    f1();
+      f2();
+    }
+    public static void f1(){
         int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void f2(){
+        int n =4;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* " );
             }
             System.out.println();
         }
