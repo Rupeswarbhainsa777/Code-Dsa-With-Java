b64c776181727a52c85788ada3b3550b310d8cce
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-17 02:48:08 +0530

    no_2 added

--- src/Pattern/F14.java
+++ src/Pattern/F14.java
@@ -85,8 +85,11 @@ f7();
     public static void f7(){
         int n =5;
         for(int i=n;i>=0;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print(" "+j);
+            for(int j=1;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print(j);
             }
             System.out.println();
         }
---
+++ src/Sliding_window/No_2.java
@@ -0,0 +1,12 @@
+package Sliding_window;
+
+import java.util.List;
+
+public class No_2 {
+    public static void main(String[] args) {
+
+    }
+    public static List<Integer> findFirstNeg(int arr[],int k){
+
+    }
+}
