9c1398dc136910d4457019cc7c10e1eaba80493e
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-17 22:44:20 +0530

    f 14 updated

--- src/Pattern/F14.java
+++ src/Pattern/F14.java
@@ -82,22 +82,18 @@ f7();
             System.out.println();
         }
     }
-    public static void f7(){
-        int n =5;
-        for(int i=n;i>=0;i--){
-            for(int j=1;j<=n;j++){
+    public static void f7() {
+        int n = 5;
+
+        for(int i=n;i>=1;i--){
+
+          for(int j=1;j<=i;j++){
+              System.out.print(j+" ");
+          }
+            for(int j=1;j<=i-n;j++){
                 System.out.print(" ");
-            }
-            for(int j=i;j<=n;j++){
-                System.out.print(j);
             }
             System.out.println();
         }
-//        for(int i=1;i<=n;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print(j+" ");
-//            }
-//            System.out.println();
-//        }
     }
 }
