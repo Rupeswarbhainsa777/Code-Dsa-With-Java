a0135b37f30c8539a032f7bccecbfdef8314b35c
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-02-23 21:44:28 +0530

    c2 addedd

--- src/Pattern/Characters.java
+++ src/Pattern/Characters.java
@@ -2,12 +2,26 @@ package Pattern;
 
 public class Characters {
     public static void main(String[] args) {
-      c1();
+//      c1();
+        c2();
     }
     public static void c1(){
 
         int n = 5;
         for(int i=1;i<=n;i++){
+            char ch ='A';
+            for (int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+    public static void c2(){
+
+        int n = 5;
+        for(int i=n;i>=1;i--){
             char ch ='A';
             for (int j=1;j<=i;j++){
                 System.out.print(ch+" ");
