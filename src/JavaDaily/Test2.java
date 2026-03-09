833a10591bef90e91abcc88a16d7e92f31cc34ff
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-10 02:09:02 +0530

    group anagram addedd

---
+++ src/JavaDaily/Test2.java
@@ -0,0 +1,23 @@
+package JavaDaily;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+
+        int arr[] = {1,3,45,6,67};
+        for(int ch : arr){
+            if(map.containsKey(ch)){
+                map.get(ch).ad
+            }
+        }
+
+
+    }
+}
---
+++ src/Leetcode_150/GroupAnagarm.java
@@ -0,0 +1,4 @@
+package Leetcode_150;
+
+public class GroupAnagarm {
+}
