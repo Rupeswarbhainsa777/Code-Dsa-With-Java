d60486b611a1889ee67ab89c95b4343a91598345
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-14 13:23:08 +0530

    top-100-coding addedd

--- src/Leetcode_150/MergeInterval.java
+++ src/Leetcode_150/MergeInterval.java
@@ -1,47 +1,35 @@
 package Leetcode_150;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
 
 public class MergeInterval {
     public static void main(String[] args) {
+        int[][] intervals = {{1,3},{4,6},{8,10},{15,18}};
 
-    }
-    public static int[][] merge(int[][] intervals) {
-
-
-        ArrayList<int []> li = new ArrayList<>();
-
-        int idx =0;
         int n = intervals.length;
 
-        if(intervals[0][0]<intervals[1][1]){
-            li.add(intervals[0]);
-            idx++;
-        }
-        n=n+1;
+        Arrays.sort(intervals[0]);
+        Arrays.sort(intervals[1]);
 
+//        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
 
-        while (idx<n &&  intervals[idx][0]>intervals[idx-1][1]){
-            li.add(intervals[idx]);
-            idx++;
-        }
-        while (idx < n && intervals[idx][0] <= intervals[idx - 1][1]) {
-
-            intervals[idx - 1][0] = Math.min(intervals[idx][0], intervals[idx - 1][0]);
-            intervals[idx - 1][1] = Math.max(intervals[idx][1], intervals[idx - 1][1]);
-
-            idx++;
-        }
-        li.add(intervals[idx]);
-
-
-        while (idx<n){
-            li.add(intervals[idx]);
-            idx++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < intervals[i].length; j++) {
+                System.out.print(intervals[i][j] + " ");
+            }
+            System.out.println();
         }
 
-        return li.toArray(new int[li.size()][]);
+
 
     }
+//    public static int[][] merge(int[][] intervals) {
+//
+//
+//
+//
+//    }
 }
