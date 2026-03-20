9a4696fba4ced08dde887edd37ef989c47174744
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-21 01:58:59 +0530

    all fixed

--- src/Leetcode_150/Testing.java
+++ src/Leetcode_150/Testing.java
@@ -4,8 +4,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Testing {
-    void main(){
-        System.out.println("Hello world");
-
+    public static void main(String[] args) {
+        byte fi = (byte) 135;
+        byte fj=12;
+        System.out.println(fj+fi);
     }
 }
