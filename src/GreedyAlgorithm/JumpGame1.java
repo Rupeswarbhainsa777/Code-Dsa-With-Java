5acf56d46b3422009b05902287b9bdccaa127290
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-05 02:42:28 +0530

    jump game 1  add

---
+++ src/GreedyAlgorithm/JumpGame1.java
@@ -0,0 +1,26 @@
+package GreedyAlgorithm;
+
+public class JumpGame1 {
+    public static void main(String[] args) {
+         int arr[] ={2,3,1,1,4};
+        System.out.println(canJump(arr));
+    }
+
+    public static boolean canJump(int[] nums) {
+
+        int max = 0;
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+
+            if (i > max) return false;
+
+
+            max = Math.max(max, i + nums[i]);
+
+        }
+        return true;
+
+    }
+}
