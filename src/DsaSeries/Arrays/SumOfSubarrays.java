dfb66e40672dd021b63685709bf96d9ebb755085
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-06 23:37:12 +0530

    sub array sum add

--- src/DsaSeries/Arrays/SumOfSubarrays.java
+++ src/DsaSeries/Arrays/SumOfSubarrays.java
@@ -1,4 +1,31 @@
 package DsaSeries.Arrays;
 
 public class SumOfSubarrays {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        sum(arr);
+
+    }
+
+    public static void sum(int arr[]) {
+
+        int n = arr.length;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                int sum = 0;
+                for (int k = j; k < j + i; k++) {
+
+                    sum += arr[k];
+                }
+                System.out.println(sum);
+            }
+
+        }
+
+    }
+
+
 }
